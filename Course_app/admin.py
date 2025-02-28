5be59dbccb1174dabcfeac8508a77c37e6e9aeb9
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-28 23:26:24 +0600

    Define models for Course, Lesson, and Student.

--- Course_app/admin.py
+++ Course_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student, Lesson, Course
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Lesson)
+admin.site.register(Course)
--- Course_app/models.py
+++ Course_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=25)
+    descriptions = models.TextField()
+    durations = models.IntegerField(help_text="Durations in hours")
+    thumbnail = models.ImageField(upload_to="courses_thumbnail/",null=True,blank=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Lesson(models.Model):
+    title = models.CharField(max_length=25)
+    content = models.TextField()
+    course = models.ForeignKey(Course,on_delete=models.CASCADE,related_name="lesson")
+    
+    def __str__(self):
+        return self.title
+    
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    roll = models.IntegerField()
+    email = models.EmailField(max_length=60)
+    enrolled_courses = models.ManyToManyField(Course,related_name="student")
+    
+    
