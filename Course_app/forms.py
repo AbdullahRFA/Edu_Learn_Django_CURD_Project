6748e56961ebe73225448fb938fa5ccfaee3bc22
AbdullahRFA <shakibrybmn@gmail.com>
2025-03-13 10:54:53 +0600

    added model form for student

--- Course_app/forms.py
+++ Course_app/forms.py
@@ -20,3 +20,14 @@ class lessonForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
+        
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['name','roll','email','enrolled_courses']
+        widgets = {
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+            'roll':forms.NumberInput(attrs={'class':'form-control'}),
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+            'enrolled_courses':forms.CheckboxSelectMultiple(attrs={'class':'form-control'})
+        }
