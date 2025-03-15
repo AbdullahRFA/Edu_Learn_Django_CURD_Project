51348fcc0ed1b645c080ae1c264ee7e72b32a2e3
AbdullahRFA <shakibrybmn@gmail.com>
2025-03-15 16:03:16 +0600

    added style on form

--- Course_app/forms.py
+++ Course_app/forms.py
@@ -39,15 +39,16 @@ class StudentForm(forms.ModelForm):
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(
         required=True,
-        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'})
+        widget=forms.EmailInput(attrs={'class': 'form-control ', 'placeholder': 'Enter your email'})
     )
+
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm your password'}),
+            'username': forms.TextInput(attrs={'class': 'form-control ', 'placeholder': 'Enter your username'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control ', 'placeholder': 'Enter your password'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control ', 'placeholder': 'Confirm your password'}),
         }
