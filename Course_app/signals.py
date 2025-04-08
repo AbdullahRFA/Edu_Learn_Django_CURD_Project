0b8ce398f7126c98fde20d9155305e41d77e22e0
AbdullahRFA <shakibrybmn@gmail.com>
2025-04-08 23:58:12 +0600

    modified signals.py for random roll number

--- Course_app/signals.py
+++ Course_app/signals.py
@@ -5,7 +5,14 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Student
 
+import random
+
 @receiver(post_save, sender=User)
 def create_student(sender, instance, created, **kwargs):
     if created:
-        Student.objects.create(user=instance, name=instance.username, email=instance.email, roll=9999)
+        Student.objects.create(
+            user=instance,
+            name=instance.username,
+            email=instance.email,
+            roll=random.randint(1000, 9999)
+        )
